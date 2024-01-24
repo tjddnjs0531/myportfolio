5e067af2aac7956f4c61ae18116d039e4a282fbe
sienna <tjddnjs0531@gmail.com>
2024-01-24 10:12:43 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// import { RouterInfo } from './RouterInfo';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/Main/ScrollToTop';
 
-// const RouterObject = createBrowserRouter(RouterInfo)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
